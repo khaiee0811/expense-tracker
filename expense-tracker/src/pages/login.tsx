4d37e2b8fca263828d5f6b0bb7f238a45148f9b3
khaieelim <s4058427@student.rmit.edu.au>
2025-07-07 19:43:40 +1000

    Added login inputs and ui layout

--- expense-tracker/src/pages/login.tsx
+++ expense-tracker/src/pages/login.tsx
@@ -2,12 +2,41 @@ import {
     Box,
     Flex,
     Heading,
+    Input,
+    FormControl,
+    FormLabel,
+    Button,
 } from "@chakra-ui/react"
 
 export default function Login() {
     return(
-        <Box>
-            <Heading>Text</Heading>
-        </Box>
+        <Flex height="100vh">
+            {/* Left: Image banner */}
+            <Box flex="1" backgroundColor="teal.400">
+
+            </Box>
+
+            {/* Right: Login Box */}
+            <Flex
+                flex="1"
+                justifyContent="center"
+                alignItems="center"
+                bg="gray.50"
+            >
+                <Box border="1px solid black" p="4em" pb="2em" borderRadius={10}>
+                    <Heading mb={8}>Login</Heading>
+                    <FormLabel>Email</FormLabel>
+                    <Input mb={8}></Input>
+                    <FormLabel>Password</FormLabel>
+                    <Input mb={7}></Input>
+                    <Flex justify="center" mb={10}>
+                        <Button width="100px" colorScheme="green">Login</Button>
+                    </Flex>
+                    <Flex justify="center">
+                        <Button fontSize="14px" variant="link">Sign Up</Button>
+                    </Flex>
+                </Box>
+            </Flex>
+        </Flex>
     ); 
 }
