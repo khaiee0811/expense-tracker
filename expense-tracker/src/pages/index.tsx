90b90fd7ce7ba02bcba68051e54de158e57ebbf6
khaieelim <s4058427@student.rmit.edu.au>
2025-07-07 18:43:01 +1000

    Added ui for the intro page/ landing page

--- expense-tracker/src/pages/index.tsx
+++ expense-tracker/src/pages/index.tsx
@@ -2,16 +2,33 @@ import {
   Box,
   Flex,
   Heading,
+  Text,
+  Button,
+  Input,
 } from "@chakra-ui/react"
-import ExpenseLogger from "@/components/ExpenseLogger";
 import Header from "@/components/Header";
 
 export default function Home() {
   return(
     <>
       <Header></Header>
-      <Flex height="100vh" justify="center" padding="2em">
-        <ExpenseLogger/>
+      <Flex 
+        justify="left"
+        align="center"
+        height="80vh"
+        px="10em"
+      >
+        <Box maxW="600px" textAlign="left">
+          <Heading fontSize="80px" mb={4} whiteSpace="nowrap">Expense Tracker</Heading>
+          <Text fontSize="20px" mb={3}>Your personalised expense tracking platform.</Text>
+          <Input 
+            variant="outline" 
+            borderColor="gray.500" 
+            mb={3}
+            placeholder="What is your name?"
+          />
+          <Button colorScheme="teal" variant="solid">Get Started</Button>
+        </Box>
       </Flex>
     </>
   );
