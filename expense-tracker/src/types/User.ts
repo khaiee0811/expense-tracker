6defde24034d3c9a2c3ee5822eedc4a4abe54092
khaieelim <s4058427@student.rmit.edu.au>
2025-07-09 20:12:14 +1000

    Created a context for authentication including login and logout function

--- expense-tracker/src/types/User.ts
+++ expense-tracker/src/types/User.ts
@@ -3,3 +3,8 @@ export type User = {
     email: string;
     password: string;
 }
+
+export const DEFAULT_USERS: User[] = [
+    { id: "1", email: "john@email.com", password: "password123"},
+    { id: "2", email: "jane@email.com", password: "password456"},
+];
